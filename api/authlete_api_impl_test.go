9121539b30155224cdd505ada621cd83e047e389
haton14 <takumi-iioka@c-fo.com>
2023-09-04 18:48:43 +0900

    AuthorizationのRequestチェック

--- api/authlete_api_impl_test.go
+++ api/authlete_api_impl_test.go
@@ -2,8 +2,10 @@ package api_test
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/authlete/authlete-go/api"
@@ -21,16 +23,42 @@ func createAuthleteConfiguration(url string) conf.AuthleteConfiguration {
 	return &cnf
 }
 
-func TestAuthorization(t *testing.T) {
+func httpHeaderCheckHelper(t *testing.T, key string, value []string) {
+	if key == "User-Agent" {
+		return
+	}
+	assert := assert.New(t)
+	wantHeader := map[string][]string{
+		"Accept":          {"application/json"},
+		"Content-Type":    {"application/json;charset=UTF-8"},
+		"Authorization":   {"Basic U0VSVklDRV9BUElfS0VZOlNFUlZJQ0VfQVBJX1NFQ1JFVA=="},
+		"Accept-Encoding": {"gzip"},
+	}
+	assert.Equal(wantHeader[key], value)
+}
+
+func TestAuthorization_Request(t *testing.T) {
 	assert := assert.New(t)
 	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// [todo] request check
-		fmt.Fprintln(w, `{"resultCode": "RESULT_CODE"}`)
+		// request check
+		for k, v := range r.Header {
+			httpHeaderCheckHelper(t, k, v)
+		}
+		assert.Equal(r.URL.Path, "/api/auth/authorization")
+		assert.Equal(r.URL.Query(), url.Values{})
+		assert.Equal(r.Method, "POST")
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			assert.NoError(err)
+		}
+		assert.Equal(string(body), "{\"parameters\":\"response_type=code\\u0026client_id=26478243745571\\u0026redirect_uri=https%3A%2F%2Fmy-client.example.com%2Fcb1\\u0026scope=timeline.read+history.read\\u0026code_challenge=E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM\\u0026code_challenge_method=S256\"}")
+		// dummy response
+		fmt.Fprintln(w, `{"dummy": "dummy"}`)
 	}))
 	defer svr.Close()
 	api, err := api.New(createAuthleteConfiguration(svr.URL))
 	assert.NoError(err)
-	resp, aErr := api.Authorization(&dto.AuthorizationRequest{Parameters: `parameters`})
+	resp, aErr := api.Authorization(&dto.AuthorizationRequest{Parameters: `response_type=code&client_id=26478243745571&redirect_uri=https%3A%2F%2Fmy-client.example.com%2Fcb1&scope=timeline.read+history.read&code_challenge=E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM&code_challenge_method=S256`})
 	assert.Nil(aErr)
 	assert.NotNil(resp)
 }
